59a2cceb87157266910036e8f06d514f197cee58
Mo Jiwa <moj@hq.trayport.com>
2023-09-26 13:15:31 +0100

    Add d20 command to the bot

---
+++ commands/fun/d20.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('d20')
+		.setDescription('Rolls a d20 dice '),
+	async execute(interaction) {        
+        const roll = getRandomNumber();
+		return interaction.reply(roll);
+	},
+};
+
+function getRandomNumber() {
+    const lowestNumber = 1;
+    const highestNumber = 20;
+    const result = Math.abs(Math.floor(Math.random() * (lowestNumber - highestNumber) + 1));
+    return result;
+}
